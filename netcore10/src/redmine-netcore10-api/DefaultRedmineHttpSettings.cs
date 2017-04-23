7be8a7c4b5af15cf642527d4a4cae07f9c21de9f
Adrian Popescu <adi.popescu@gmail.com>
2017-04-23 23:37:04 +0300

    Added ServerCertificateCustomValidationCallback, refactoring.

--- netcore10/src/redmine-netcore10-api/DefaultRedmineHttpSettings.cs
+++ netcore10/src/redmine-netcore10-api/DefaultRedmineHttpSettings.cs
@@ -1,7 +1,10 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Net.Security;
 using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Redmine.Net.Api
 {
@@ -10,11 +13,6 @@ namespace Redmine.Net.Api
         private DefaultRedmineHttpSettings()
         {
             DecompressionMethods = DecompressionMethods.Deflate | DecompressionMethods.GZip | DecompressionMethods.None;
-        }
-
-        public static DefaultRedmineHttpSettings Builder()
-        {
-            return new DefaultRedmineHttpSettings();
         }
 
         public IWebProxy WebProxy { get; private set; }
@@ -29,7 +27,14 @@ namespace Redmine.Net.Api
         public bool UseDefaultCredentials { get; private set; }
         public bool UseCookies { get; private set; }
         public bool PreAuthenticate { get; private set; }
-        
+
+        public bool VerifyServerCertificate { get; private set; } = true;
+
+        public Func<HttpRequestMessage, X509Certificate2, X509Chain, SslPolicyErrors, bool> ServerCertificateCustomValidationCallback {
+            get;
+            private set;
+        }
+
         public IRedmineHttpSettings SetWebProxy(IWebProxy webProxy)
         {
             WebProxy = webProxy;
@@ -103,32 +108,88 @@ namespace Redmine.Net.Api
         }
 
 
+        public IRedmineHttpSettings Set(Func<HttpRequestMessage, X509Certificate2, X509Chain, SslPolicyErrors, bool>
+                serverCertificateCustomValidationCallback)
+        {
+            ServerCertificateCustomValidationCallback = serverCertificateCustomValidationCallback;
+            return this;
+        }
+
         public HttpClientHandler Build()
         {
-            HttpClientHandler handler = new HttpClientHandler();
+            var handler = new HttpClientHandler();
 
-            handler.AutomaticDecompression = DecompressionMethods;
-            handler.CookieContainer = CookieContainer;
+            if (handler.SupportsAutomaticDecompression)
+                handler.AutomaticDecompression = DecompressionMethods;
 
-            if (UseCookies && handler.CookieContainer == null)
-                handler.CookieContainer = new CookieContainer();
-
-            handler.Proxy = WebProxy;
-            handler.UseProxy = UseProxy;
-
-            if (UseProxy && WebProxy != null)
+            if (handler.SupportsRedirectConfiguration)
             {
-                handler.Proxy.Credentials = ProxyCredentials;
+                //TODO:
             }
 
-            handler.Credentials = DefaultCredentials;
+            if (UseCookies)
+            {
+                handler.UseCookies = UseCookies;
+
+                //TODO: add default cookieContainer
+                handler.CookieContainer = CookieContainer;
+            }
+
+            if (UseProxy)
+            {
+                handler.UseProxy = UseProxy;
+                if (handler.SupportsProxy)
+                {
+                    handler.Proxy = WebProxy;
+
+                    if (UseProxy && WebProxy != null)
+                        handler.Proxy.Credentials = ProxyCredentials;
+                }
+            }
+
+            if (UseDefaultCredentials)
+            {
+                handler.UseDefaultCredentials = UseDefaultCredentials;
+                handler.Credentials = DefaultCredentials;
+            }
+
             handler.PreAuthenticate = PreAuthenticate;
 
+            if (SslProtocols == default(SslProtocols))
+            {
+            //   handler.SslProtocols 
+            }
+
             handler.SslProtocols = SslProtocols;
-            handler.UseCookies = UseCookies;
-            handler.UseDefaultCredentials = UseDefaultCredentials;
+
+            if (VerifyServerCertificate)
+            {
+                if (ServerCertificateCustomValidationCallback == null)
+                {
+                    handler.ServerCertificateCustomValidationCallback += (message, certificate2, chain, sslErrors) =>
+                    {
+                        if (sslErrors == SslPolicyErrors.None)
+                        {
+                            return true;
+                        }
+
+                        return false;
+                    };
+                }
+                else
+                {
+                    handler.ServerCertificateCustomValidationCallback = ServerCertificateCustomValidationCallback;
+                }
+            }
+
+            
 
             return handler;
+        }
+
+        public static DefaultRedmineHttpSettings Builder()
+        {
+            return new DefaultRedmineHttpSettings();
         }
     }
 }
